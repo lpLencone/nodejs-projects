ca02908e382843c56febdcab159a94f47e460d7e
ConstructAware <lp.lencone@gmail.com>
2022-04-01 11:48:32 -0300

    setting connection to database

--- 4-jobs-API/app.js
+++ 4-jobs-API/app.js
@@ -3,6 +3,13 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+// import database connector
+const connectDatabase = require('./db/connectDatabase')
+
+// requiring routes
+const authRouter = require('./routes/authRouter')
+const jobsRouter = require('./routes/jobsRouter')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -11,9 +18,8 @@ app.use(express.json())
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api')
-})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -22,9 +28,9 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    )
+    // connect to database
+    await connectDatabase(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error)
   }
